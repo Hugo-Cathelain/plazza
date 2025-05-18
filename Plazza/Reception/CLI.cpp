31e0415e73f53ac3167052a68a0e6a7b6f77e6c3
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-18 20:46:47 +0200

    add: adding base for the CLI

---
+++ Plazza/Reception/CLI.cpp
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Reception/CLI.hpp"
+#include "Reception/Parser.hpp"
+#include <iostream>
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace Plazza
+///////////////////////////////////////////////////////////////////////////////
+namespace Plazza
+{
+
+///////////////////////////////////////////////////////////////////////////////
+CLI::CLI(Reception& reception)
+    : m_reception(reception)
+{}
+
+///////////////////////////////////////////////////////////////////////////////
+void CLI::DisplayPrompt(void) const
+{
+    std::cout << "> ";
+    std::cout.flush();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void CLI::Run(void)
+{
+    std::string line;
+    std::cout << "Welcome to Plazza!" << std::endl;
+    std::cout << "Enter pizza orders "
+              << "(e.g., 'regina XXL x1; margarita S x2') "
+              << "or 'status' or 'exit'."
+              << std::endl;
+
+    while (true)
+    {
+        DisplayPrompt();
+
+        if (!std::getline(std::cin, line))
+        {
+            std::cout << "Exiting Plazza." << std::endl;
+            break;
+        }
+
+        std::string command = Parser::Trim(line);
+
+        if (command.empty())
+        {
+            continue;
+        }
+
+        if (command == "exit")
+        {
+            std::cout << "Exiting Plazza." << std::endl;
+            break;
+        }
+
+        ProcessCommand(command);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void CLI::ProcessCommand(const std::string& line)
+{
+    if (line == "status")
+    {
+        // TODO: Add reception DisplayStatus function
+    }
+    else
+    {
+        try
+        {
+            Parser::Orders orders = Parser::ParseOrders(line);
+
+            if (!orders.empty())
+            {
+                std::cout << "Order received. Processing..." << std::endl;
+                // TODO: Add reception HandleOrder
+            }
+        }
+        catch (const std::runtime_error& e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+}
+
+} // !namespace Plazza
