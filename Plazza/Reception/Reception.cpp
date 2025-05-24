c63eca9ffaef73971a6dd16edfef6fbc601bc27c
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-24 15:10:38 +0200

    add: adding code

--- Plazza/Reception/Reception.cpp
+++ Plazza/Reception/Reception.cpp
@@ -33,6 +33,10 @@ Reception::Reception(std::chrono::milliseconds restockTime, size_t CookCount)
     m_windowThread.Start();
 #endif
     CreateKitchen();
+    CreateKitchen();
+    CreateKitchen();
+    CreateKitchen();
+    CreateKitchen();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -170,45 +174,176 @@ void Reception::ProcessOrders(const Parser::Orders& orders)
 ///////////////////////////////////////////////////////////////////////////////
 void Reception::WindowRoutine(void)
 {
-    sf::RenderWindow window(sf::VideoMode(985, 515), "Reception");
-    sf::Event event;
-    sf::Texture background;
+    const float RECEPTION_HEIGHT = 515.f;
+    const float KITCHEN_HEIGHT = 390.f;
+    const float WINDOW_WIDTH = 985.f;
+    const float SCROLL_SPEED = 30.f;
+    const float SCREEN_MARGIN = 200.f;
 
-    if (!background.loadFromFile("Assets/Images/Reception.png"))
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    const float screenApiHeight = static_cast<float>(desktop.height);
+
+    auto get_view_and_content_heights =
+        [&](size_t p_kitchenCount) -> std::pair<float, float>
+    {
+        float contentH = RECEPTION_HEIGHT + p_kitchenCount * KITCHEN_HEIGHT;
+        float desiredWindowH = contentH;
+
+        float maxPossibleWindowH = screenApiHeight - SCREEN_MARGIN;
+
+        float windowH = std::min(desiredWindowH, maxPossibleWindowH);
+        windowH = std::max(windowH, std::min(RECEPTION_HEIGHT, maxPossibleWindowH));
+        windowH = std::max(windowH, 100.f);
+
+        return {contentH, windowH};
+    };
+
+    size_t kitchenCount = m_kitchens.size();
+
+    auto initial_dims = get_view_and_content_heights(kitchenCount);
+    float currentTotalContentHeight = initial_dims.first;
+    float actualWindowHeight = initial_dims.second;
+
+    sf::RenderWindow window(sf::VideoMode(
+        static_cast<unsigned int>(WINDOW_WIDTH),
+        static_cast<unsigned int>(actualWindowHeight)
+    ), "The Plazza", sf::Style::Default);
+    sf::View view;
+    sf::Event event;
+    sf::Texture backgroundTexture;
+    sf::Texture kitchensTexture;
+
+    if (!kitchensTexture.loadFromFile("Assets/Images/Kitchens.png"))
     {
         return;
     }
 
-    sf::Sprite sprite(background);
+    if (!backgroundTexture.loadFromFile("Assets/Images/Reception.png"))
+    {
+        return;
+    }
+
+    sf::Sprite kitchenSprite(kitchensTexture);
+    sf::Sprite backgroundSprite(backgroundTexture);
     sf::Music music;
 
-    if (!music.openFromFile("Assets/Musics/Menu.ogg"))
+    if (!music.openFromFile("Assets/Musics/Game.ogg"))
     {
         return;
     }
+
+    view.setSize(sf::Vector2f(WINDOW_WIDTH, actualWindowHeight));
+    if (currentTotalContentHeight <= actualWindowHeight)
+    {
+        view.setCenter(WINDOW_WIDTH / 2.f, currentTotalContentHeight / 2.f);
+    }
+    else
+    {
+        view.setCenter(WINDOW_WIDTH / 2.f, actualWindowHeight / 2.f);
+    }
+    window.setView(view);
 
     music.setLoop(true);
     music.play();
 
-    while (m_windowThread.running && !m_shutdown)
+    while (m_windowThread.running && !m_shutdown && window.isOpen())
     {
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {}
+            {
+                window.close();
+            }
+            else if (event.type == sf::Event::MouseWheelScrolled)
+            {
+                if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
+                {
+                    float liveContentHeight = RECEPTION_HEIGHT + kitchenCount * KITCHEN_HEIGHT;
+
+                    float currentViewHeight = view.getSize().y;
+
+                    if (liveContentHeight > currentViewHeight)
+                    {
+                        float scrollDelta = -event.mouseWheelScroll.delta * SCROLL_SPEED;
+
+                        sf::Vector2f center = view.getCenter();
+                        float newCenterY = center.y + scrollDelta;
+
+                        float viewHalfHeight = currentViewHeight / 2.f;
+                        float minCenterY = viewHalfHeight;
+                        float maxCenterY = liveContentHeight - viewHalfHeight;
+
+                        newCenterY = std::max(minCenterY, std::min(newCenterY, maxCenterY));
+
+                        view.setCenter(center.x, newCenterY);
+                        window.setView(view);
+                    }
+                }
+            }
         }
 
+        if (!window.isOpen())
+        {
+            break;
+        }
+
+        size_t newKitchenCountSnapshot = m_kitchens.size();
+        if (newKitchenCountSnapshot != kitchenCount)
+        {
+            auto new_dims = get_view_and_content_heights(newKitchenCountSnapshot);
+            float newContentTotalHeight = new_dims.first;
+            float newActualWindowHeight = new_dims.second;
+
+            sf::Vector2f oldViewCenter = view.getCenter();
+            sf::Vector2f oldViewSize = view.getSize();
+
+            window.setSize(sf::Vector2u(static_cast<unsigned int>(WINDOW_WIDTH), static_cast<unsigned int>(newActualWindowHeight)));
+            view.setSize(sf::Vector2f(WINDOW_WIDTH, newActualWindowHeight));
+
+            float oldViewTop = oldViewCenter.y - oldViewSize.y / 2.f;
+            float targetNewCenterY = oldViewTop + newActualWindowHeight / 2.f;
+
+            float viewHalfHeightNew = newActualWindowHeight / 2.f;
+            float minCenterY = viewHalfHeightNew;
+            float maxCenterY = newContentTotalHeight - viewHalfHeightNew;
+
+            if (newContentTotalHeight <= newActualWindowHeight)
+            {
+                targetNewCenterY = newContentTotalHeight / 2.f;
+            }
+            else
+            {
+                targetNewCenterY = std::max(minCenterY, std::min(targetNewCenterY, maxCenterY));
+            }
+
+            view.setCenter(sf::Vector2f(WINDOW_WIDTH / 2.f, targetNewCenterY));
+            window.setView(view);
+
+            kitchenCount = newKitchenCountSnapshot;
+            currentTotalContentHeight = newContentTotalHeight;
+        }
+
+        backgroundSprite.setPosition(sf::Vector2f(0.0f, kitchenCount * KITCHEN_HEIGHT));
+
         window.clear();
-        window.draw(sprite);
+        window.draw(backgroundSprite);
+
+        for (size_t i = 0; i < kitchenCount; i++)
+        {
+            kitchenSprite.setPosition(sf::Vector2f(0.0f, (kitchenCount - 1 - i) * KITCHEN_HEIGHT));
+
+            kitchenSprite.setTextureRect(sf::IntRect(
+                sf::Vector2i(0, (m_kitchens[i]->GetID() % 20) * static_cast<int>(KITCHEN_HEIGHT)),
+                sf::Vector2i(static_cast<int>(WINDOW_WIDTH), static_cast<int>(KITCHEN_HEIGHT))
+            ));
+
+            window.draw(kitchenSprite);
+        }
+
         window.display();
     }
 
     music.stop();
-
-    if (window.isOpen())
-    {
-        window.close();
-    }
 }
 #endif
 
